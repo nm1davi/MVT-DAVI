b7cb9619b3c87f4f9157c623f42c3a1054f04df2
Nicolas Davi <nm1.davi@gmail.com>
2022-06-23 22:48:38 -0300

    Otro

--- MTVDAVI/views.py
+++ MTVDAVI/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
+from django.template import Template, context
 
 def inicio(request):
     return HttpResponse("Hola soy la vista de Django")
 
 def mi_template(request):
-    return HttpResponse("Mi template")
+    
+    archivo = open(r"C:\Users\USUARIO\Desktop\TrabajoMTV\templates\prueba.html", "r")
+    
+    template1 = Template(archivo.read())
+    
+    contexto1 = context()
+    
+    render1 = template1.render(contexto1)
+    
+    return HttpResponse(render1)
