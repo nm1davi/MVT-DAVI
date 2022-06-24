1ec5bb92b3bfe436225a466b3004b0c0c754ff23
Nicolas Davi <nm1.davi@gmail.com>
2022-06-23 22:33:24 -0300

    Otro arreglo

--- MTVDAVI/urls.py
+++ MTVDAVI/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .views import inicio, mi_template
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', inicio),
+    path('mi-template/', mi_template),
+
 ]
--- MTVDAVI/views.py
+++ MTVDAVI/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
+def inicio(request):
+    return HttpResponse("Hola soy la vista de Django")
+
 def mi_template(request):
     return HttpResponse("Mi template")
